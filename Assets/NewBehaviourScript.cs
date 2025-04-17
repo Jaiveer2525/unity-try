f9fa244f084796399ba5876d60758895fd5ff6db
Jaiveer2525 <jayeshbisht25@gmail.com>
2025-04-17 15:50:07 +0530

    Bad movment controller

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    private Vector2 hori_velocity;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
-       {
-           gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 1000, ForceMode.Impulse);
-       } 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        hori_velocity.x *= (float)0.0;
         
+        if (Input.GetKeyDown(KeyCode.Space) && rb.linearVelocity.y <= 0)
+        {
+            rb.AddForce(Vector2.up * 200);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (hori_velocity.x > 0)
+            {
+                hori_velocity.x = -hori_velocity.x;
+            }
+            hori_velocity += Vector2.left * 100;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (hori_velocity.x < 0)
+            {
+                hori_velocity.x = -hori_velocity.x;
+            }
+            hori_velocity += Vector2.right * 100;
+        }
+        
+        rb.AddForce(hori_velocity);
     }
 }
